2037e6e9943c394e1dc7185d959febfd5fda2fb6
Tushar agrawal <gargtushar495@gmail.com>
2023-10-08 14:49:59 +0530

    add notes

---
+++ types/type_anotations.ts
@@ -0,0 +1,5 @@
+// in typescript type anotations is way to explicitly specifying the type of the variable or a function parameter
+// or function return value.
+
+var myName:string = "Tushar agrawal"
+console.log(myName);
