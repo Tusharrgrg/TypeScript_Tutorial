277565ca5311b8894a9863e6bdc53842347c5dbb
Tushar agrawal <gargtushar495@gmail.com>
2023-10-08 15:28:18 +0530

    add code folder

--- types/type_anotations.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// in typescript type anotations is way to explicitly specifying the type of the variable or a function parameter
-// or function return value.
-var myName = "Tushar agrawal";
-console.log(myName);
--- types/type_anotations.ts
+++
@@ -1,5 +0,0 @@
-// in typescript type anotations is way to explicitly specifying the type of the variable or a function parameter
-// or function return value.
-
-var myName:string = "Tushar agrawal"
-console.log(myName);
