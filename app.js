bc43c8d1db17ce68454224b1436de356c4d8e3fe
Tushar agrawal <gargtushar495@gmail.com>
2023-10-08 23:23:37 +0530

    add notes

--- app.js
+++ app.js
@@ -1,2 +1,38 @@
 "use strict";
-console.log("Here we go >>>>");
+const input1 = document.getElementById("num1");
+const input2 = document.getElementById("num2");
+const proces = document.querySelector("button");
+function add(a, b) {
+    return a + b;
+}
+const element = document.getElementById("add_result");
+// this shows button's behaviour on clich that button
+// proces?.addEventListener('click', function () {
+//     if (element) {
+//         element.textContent = " The Result is : " + add(+input1.value, +input2.value).toString();
+//     }
+// })
+// also we can define a object like this in typescript
+// const personDemo : {
+//     name : string,
+//     age : number
+// } ={
+//     name : "Tushar",
+//     age : 21
+// }
+// this sis an object datatype in typescript
+const person = {
+    name: "Tushar",
+    age: 21,
+    hobbies: ["Sports", "Music", "Art & Craft"]
+};
+console.log(person.name);
+let dummy = function (a, b) {
+    if (typeof a === "number" && b === "number") {
+        return a + b;
+    }
+    else {
+        return a.toString() + b.toString();
+    }
+};
+console.log(dummy("tushar", 5));
--- app.ts
+++ app.ts
@@ -1,1 +1,45 @@
-console.log("Here we go >>>>");
+const input1 = document.getElementById("num1")! as HTMLInputElement
+const input2 = document.getElementById("num2")! as HTMLInputElement
+const proces = document.querySelector("button")
+
+function add(a: number, b: number) {
+    return a + b;
+}
+
+const element = document.getElementById("add_result");
+
+// this shows button's behaviour on clich that button
+// proces?.addEventListener('click', function () {
+//     if (element) {
+//         element.textContent = " The Result is : " + add(+input1.value, +input2.value).toString();
+//     }
+// })
+
+// also we can define a object like this in typescript
+// const personDemo : {
+//     name : string,
+//     age : number
+// } ={
+//     name : "Tushar",
+//     age : 21
+// }
+
+// this sis an object datatype in typescript
+const person = {
+    name : "Tushar",
+    age : 21,
+    hobbies : ["Sports", "Music", "Art & Craft"]
+}
+
+console.log(person.name);
+
+// union operator to use more then one type operator
+let dummy = function(a:string | number , b : string | number) {
+    if(typeof a === "number" && b === "number"){
+        return a+b;
+    }else{
+        return a.toString() + b.toString();
+    }
+}
+
+console.log(dummy("tushar", 5));
